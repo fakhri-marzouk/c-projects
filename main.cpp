d86d848a3cc3bde6f3a0bbaffd4f9a69cf7ae32a
houda sayar <houdasayar12@gmail.com>
2021-04-25 12:46:02 -0700

    houda commit1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+#include<ostream>
+#include<istream>
+#include"bib.h"
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
