638d0524dca428a9e38f87c44bd97f059bee5a98
ines_kr <ineskrichene87@gmail.com>
2021-05-04 19:06:51 +0100

    ines commit2 authentification system

--- comptes.h
+++ comptes.h
@@ -1,37 +1,113 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 using namespace std;
 
 class compte{
 protected:
     string pseudo;
-    string pass;
+    string password;
 public:
+    //==
+    bool operator==(const compte&);
     compte(string Pseudo="", string Pass=""){
-        this->pass=Pass;
+        this->password=Pass;
         this->pseudo=Pseudo;
     }
-    ~compte(){
-        delete this;
-    }
     string getPseudo(){return this->pseudo;}
-    string getPass(){return this->pass;}
+    string getPass(){return this->password;}
     void setPseudo(string Pseudo=""){this->pseudo=Pseudo;}
-    void setPass(string Pass=""){this->pass=Pass;}
+    void setPass(string Pass=""){this->password=Pass;}
 
+
+    friend istream& operator>>(istream& in,compte& c){
+        cout<<"Votre pseudo : "<<endl;
+        cin>>c.pseudo;
+        cout<<"Votre mot de passe : "<<endl;
+        cin>>c.password;
+    }
 };
+
+bool compte::operator==(const compte& c){
+    bool equal=0;
+    if((this->getPseudo()==c.pseudo)&&(this->getPass()==c.password)){
+        equal=1;
+    }
+    return equal;
+}
+
+class comptes{
+private:
+    vector<compte> table;
+public:
+    int find(compte);
+    bool ajouter(compte);
+    bool verifier(compte);
+    bool supprimer(compte);
+    
+};
+int comptes::find(compte c){
+    int numCompte=-1;
+    if(this->verifier(c)){
+        for(int i=0;i<(this->table).size();i++){
+            if(c==(this->table)[i]){
+                numCompte=i;
+            }
+        }
+    }   
+    return numCompte;
+}
+bool comptes::verifier(compte c){
+    for(unsigned int i=0;i<(this->table).size();i++){
+        if(c==(this->table)[i]){
+            return 1;
+        }
+        else continue;
+    }
+    return 0;
+}
+bool comptes::ajouter(compte c){
+    (this->table).push_back(c);
+    return 1;
+}
+bool comptes::supprimer(compte c){
+    int position=this->find(c);
+    if (position!=-1){
+        (this->table).erase((this->table). begin() + position);
+    return 1;
+    }
+    else return 0;
+}
+
 class personne{
 protected:
     string pseudo;
+    string type;
     string nom;
     string prenom;
+    string type;//checklist?
     int CIN;
     string adresse;
     string mail;
     int tel;
+    //file to add
 public:
+//constructors
+    personne(){
+        this->pseudo="";
+        this->type="";
+        this->nom="";
+        this->prenom="";
+        this->type="";
+        this->CIN=0;
+        this->adresse="";
+        this->mail="";
+        this->tel=0;
+    }
+    personne(string,string,string,string,string);
+
+//getters
+    string getType(){return this->type;}
     string getPseudo(){return this->pseudo;}
     string getNom(){return this->nom;}
     string getPrenom(){return this->prenom;}
@@ -39,6 +115,8 @@ public:
     string getAdresse(){return this->adresse;}
     string getMail(){return this->mail;}
     int getTel(){return this->tel;}
+//setters
+    void setType(string Type=""){this->type=Type;}
     void setPseudo(string Pseudo=""){this->pseudo=Pseudo;}
     void setNom(string Nom=""){this->nom=Nom;}
     void setPrenom(string Prenom=""){this->prenom=Prenom;}
@@ -46,47 +124,55 @@ public:
     void setAdresse(string Adresse=""){this->adresse=Adresse;}
     void setMail(string Mail=""){this->mail=Mail;}
     void setTel(int Tel=0){this->tel=Tel;}
-    void identifier(compte);
-    void saisir();
-
-};
-class employe: public personne{
-    
-};
-class fournisseur: public personne{
-
-};
-class client: public personne{
-
-};
-class gerant: public personne{
+//methods
+    bool identifier(compte);
+    void saisir();//cin>>
 
 };
 
-class tableComptes{
-    vector<personne> t;
+class personnes{
+    vector<personne> table;
 public:
-    tableComptes(){
+    personnes(){
         
     }
-    personne getPersonne();
+    bool existe(compte);
+    personne getPersonne(compte);
     void setPersonne(personne p);
     void ajouter(personne p);
     void supprimer(personne p);
-    ~tableComptes(){
+    ~personnes(){
         delete[]this;
     }
 };
+bool personnes::existe(compte c){
+    bool test=0;
+    for(int i=0;i<(this->table).size();i++){
+        if ((this->table)[i].identifier(c)){
+            test=1;
+            break;
+        }
+    }
+    return test;
+}
 
-
-
-void personne::identifier(compte c){
-    this->pseudo=c.getPseudo();
+personne personnes::getPersonne(compte c){
+    if (this->existe(c)){
+        for(int i=0;i<(this->table).size();i++){
+            if ((this->table)[i].identifier(c)){
+                return (this->table)[i];
+            }
+        }
+    }
+    
+}
+bool personne::identifier(compte c){
+    if (this->getPseudo()==c.getPseudo())
+    return 1;
+    else return 0;
 }
 
 void personne::saisir(){
-    cout<<"pseudo: ";
-    cin>>this->pseudo;
     cout<<endl<<"nom: ";
     cin>>this->nom;
     cout<<endl<<"prénom: ";
@@ -100,3 +186,4 @@ void personne::saisir(){
     cout<<endl<<"Tel: ";
     cin >> this->tel;
 }
+
--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
 #include"comptes.h"
 
 int main(){
+    comptes tabComptes;
+    personnes tabPersonnes;
+    personne p;
+    compte c;
     while(1){
+        bool loggedIn=0;
         cout<<"voulez-vous s'authentifier ou créer un compte?"<<endl;
-        cout<<"1 : authentifer"<<endl;
+        cout<<"1 : s'authentifier"<<endl;
         cout<<"2 : créer un compte"<<endl;
         cout<<"Votre choix : ";
         int choix;
         cin>> choix;
         system("pause<0");
-        break;
-        if (choix==1){
-            
+        
+        switch (choix){
+        case 1:{
+            cout<<"Authentification : "<<endl;
+            cin>>c;
+            if(tabComptes.verifier(c)){
+                loggedIn=1;
+                personne p=tabPersonnes.getPersonne(c);
+            }
+            break;
         }
-        else if (choix==2){
+        case 2:{
             cout<<"Choisissez le type du compte à créer :"<<endl;
             cout<<"1 : client"<<endl;
             cout<<"2 : fournisseur"<<endl;
@@ -22,31 +34,66 @@ int main(){
             cout<<"Votre choix : ";
             cin>> choix;
             //creation de compte
-            if (choix==1){
-                client p;
-            }
-            else if (choix==2){
-                fournisseur p;
-            }
-            else if (choix==3){
-                employe p;
-            }
-            else if (choix==4){
-                gerant p;
-            }
-            personne p;
+            switch(choix){
+                case 1:{
+                    p.setType("client");
+                    break;
+                }
+                case 2:{
+                    p.setType("fournisseur");
+                    break;
+                }
+                case 3:{
+                    p.setType("employe");
+                    break;
+                }
+                case 4:{
+                    p.setType("gerant");
+                    break;
+                }
+                default:{
+                    cout<<"choix non existant"<<endl;
+                    break;
+                }
+            }//fin switch pour choisir type
+            //creation d'une personne
+            cout<<"création d'un compte"<<endl;
+            string Pseudo="";
+            string Password="";
+            do{
+                cin>>Pseudo;
+                c.setPseudo();
+            }while(tabComptes.find(c)!=-1);
+            cin>>Password;
+            c.setPass();
+            tabComptes.ajouter(c);
             p.saisir();
-
+            tabPersonnes.ajouter(p);
         }
+        default:
+            break;
+        }
+        if(loggedIn){
+            if (p.getType()=="client"){
+                //affichage pour client
 
-    /*string pseudo;
-    string nom;
-    string prenom;
-    int CIN;
-    string adresse;
-    string mail;
-    long tel;*/
-        
+            }else if(p.getType()=="fournisseur"){
+                //affichage pour fournisseur
+
+            }else if(p.getType()=="employe"){
+                //affichage pour employe
+
+            }else if(p.getType()=="gerant"){
+                //affichage pour gerant
+
+            }else{
+                //logged out
+
+            }
+        }
+        else{
+            continue;
+        }        
     }
     return 0;
 }
